843fb2783dd98dfe8f30964978ca635512f86db8
Rafapp <rpadiper@gmail.com>
2023-01-06 14:56:10 -0600

    learning c++

---
+++ learning.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Standard print
+    cout << "Hello World!\n"; 
+
+    // With variable
+    const int age = 21;
+    cout << "My age is: " << age << "\n";
+
+
+    return 0;
+}
