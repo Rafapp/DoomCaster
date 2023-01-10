b3ca1f73ce819e77e73a5e2219dd18d06539b1cd
Rafapp <rpadiper@gmail.com>
2023-01-10 16:39:43 -0600

    learning progress

--- learning.cpp
+++ learning.cpp
@@ -4,6 +4,49 @@
 
 using namespace std;
 
+// Default parameters
+void PrintPlayerName(string pname = "DoomGuy"){
+    cout << pname << "\n";
+}
+
+// Function that passes by reference to swap
+void SwapPlayers(string &player1, string &player2){
+    string temp = player1;
+    player1 = player2;
+    player2 = temp;
+}
+
+// Function overloading, many functions with same name!
+void SwapPlayers(string player1, string player2, int swapSpeed){}
+void SwapPlayers(string player1, string player2, int swapSpeed, float swapFrequency){}
+
+// OOP
+class DoomPlayer{
+    public: 
+        string playerName;
+        int playerLevel;
+        // Constructor
+        DoomPlayer(){
+            cout << "Doom player constructor called" << "\n";
+        }
+        // Function for class from inside
+        void printPlayer(){
+            cout << playerName << ", " << playerLevel << "\n";
+        }
+        void PrintPlayerFunny();
+};
+// Function for class from outside
+void DoomPlayer::PrintPlayerFunny(){
+    cout << "Poopy" << playerName << "\n";
+};
+// Inheritance
+class SuperDoomPlayer:DoomPlayer{
+    public:
+        SuperDoomPlayer(){
+            cout << "SUPER!";
+        }
+};
+
 int main() {
     // Standard print
     cout << "Hello World!\n"; 
@@ -102,5 +145,70 @@ int main() {
     // Array size
     cout << "The previous array has byte size: " << sizeof(nums) << "\n";
     cout << "Dividing that by data type (int), we have element size: " << sizeof(nums) / sizeof(int) << "\n";
+
+    // 2D Arrays (matrix)
+    int map[3][3] = {
+        {0,1,0},
+        {0,1,0},
+        {0,0,0}
+    };
+
+    // Structs (classes)
+    // Unnamed structure
+    struct{
+        int hp;
+        string gamertag;
+    } player1, player2, player3;
+    player1.hp = 100;
+    player1.gamertag = "TheRaff";
+    cout << "HP, Tag: " << player1.hp << ", " << player1.gamertag << "\n";
+    
+    // Named structure
+    struct Player{
+        int hp;
+        string gamertag;
+    }; // Don't forget the semicolon!
+    Player namedPlayer;
+    namedPlayer.hp = 75;
+    namedPlayer.gamertag = "TheRaff";
+    cout << "HP, Tag: " << namedPlayer.hp << ", " << namedPlayer.gamertag << "\n";
+
+    // References (only store address)
+    string playerVar = "Demon";
+    string &classRef = playerVar;
+    cout << "Var: " << playerVar << ", ref to var: " << classRef << "\n";
+
+    // Memory address print is same as C
+    cout << &classRef << "\n";
+
+    // Pointers (store address + value)
+    string* ptr = &playerVar;
+    *ptr = "Human";
+    cout << playerVar << "\n";
+
+    // Printing player name with default params
+    PrintPlayerName();
+    PrintPlayerName("DoomGuyTwo");
+
+    // Passing by reference, 1 is human 2 is demon we swap
+    string playerVar2 = "Demon";
+    cout << "Player1, Player2 b4 swap: " << playerVar << ", " << playerVar2 << "\n";
+    SwapPlayers(playerVar, playerVar2);
+    cout << "Player1, Player2 after swap: " << playerVar << ", " << playerVar2 << "\n";
+
+    
+    DoomPlayer rafa;
+    rafa.playerName = "TheRaff";
+    rafa.playerLevel = 999;
+    rafa.printPlayer();
+    rafa.PrintPlayerFunny();
+
+    SuperDoomPlayer superRafa;
+
+    // Access specifiers
+    // Private: Members accessible outside
+    // Public: Members accessible within
+    // Protected: Members accessible within, and when inherited
+
     return 0;
 }
